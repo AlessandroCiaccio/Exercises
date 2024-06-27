082c617c1346a7c24d29e569e2f29725eee03b00
Alessandro Scrofani <sandroscro@libero.it>
2024-06-27 17:26:07 +0200

    canceledMagazzino

--- Magazzino/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        WarehouseManager mywarehouse = new WarehouseManager();
-        Menu menu = new Menu();
-        Cart cart = new Cart();
-
-        menu.searchBy(cart);
-    }
-}
